e657e8472255021d911420bccb0d83a05d11bbf4
anamolhasan <mdanamolhasan1998@gmail.com>
2025-07-28 15:47:19 +0600

    basic ejs

--- templete-engine-ejs/index.js
+++ templete-engine-ejs/index.js
@@ -1,12 +1,18 @@
 const express = require('express')
-
 const app = express()
 const port = 3000
+const path = require('path')
+const { title } = require('process')
 
+// set EJS as the template engine
+app.set('view engine', 'ejs')
+
+// set views directory (folder containing)
+app.set('views', path.join(process.cwd(), 'views'))
 
 
 app.get('/', (req, res) => {
-  res.send('Server is running ........')
+  res.render('index', {title: 'Home Page', message: 'Welcome to EJS'})
 })
 
 
