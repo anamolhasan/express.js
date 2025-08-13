316ec8dbbffdeb100aa2fd2ef965d027a164b7ba
anamolhasan <mdanamolhasan1998@gmail.com>
2025-08-13 12:18:13 +0600

    custom error

--- error-handling/index.js
+++ error-handling/index.js
@@ -2,12 +2,23 @@ const express = require('express')
 const app = express()
 const fs = require('fs')
 const errorHandler = require('./src/middleware/errorHandler')
+const CustomError = require('./src/middleware/customError')
 const port = process.env.PORT || 3000
 
 
 app.get('/', (req, res, next) => {
     const error = new Error('Home route error')
     next(error)
+})
+
+// not found route
+app.get('/not-fount', (req, res, next) => {
+    next(new CustomError('Resource not found!', 404))
+})
+
+// unauthorized route
+app.get('/unauthorized', (req, res, next) => {
+    next(new CustomError('Unauthorized access!', 401))
 })
 
 app.get('/about', (req, res, next) => {
--- error-handling/src/middleware/errorHandler.js
+++ error-handling/src/middleware/errorHandler.js
@@ -1,11 +1,28 @@
 
 // error middleware
+// const errorHandler = (err, req, res, next) => {
+//     const statusCode = err.statusCode || 500
+//     const message = err.message || 'Internal Server Error'
+//     res.status(statusCode).join({
+//         success: false,
+//         message,
+//     })
+// }
+// module.exports = errorHandler
+
+const CustomError = require("./customError")
+
 const errorHandler = (err, req, res, next) => {
-    const statusCode = err.statusCode || 500
-    const message = err.message || 'Internal Server Error'
-    res.status(statusCode).join({
+    if(err instanceof CustomError){
+        return res.status(err.statusCode).json({
+            success: false,
+            message: err.message
+        })
+    }
+
+    res.status(500).json({
         success: false,
-        message,
+        message: 'Internal Server Error'
     })
 }
 module.exports = errorHandler
